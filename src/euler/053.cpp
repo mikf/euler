76d6c767229bde11605f45558b2c64685f494fa2
Mike Fährmann <mike_faehrmann@web.de>
2014-02-26 23:24:44 +0100

    Problem 053: Adjusted problem description

--- src/euler/053.cpp
+++ src/euler/053.cpp
@@ -15,9 +15,10 @@ using namespace math;
 //      C(n, r) = (n!) / (r! * (n−r)!)
 // , where r ≤ n, n! = n × (n−1) × ... × 3 × 2 × 1, and 0! = 1.
 //
-// It is not until n = 23, that a value exceeds one-million: 23C10 = 1144066.
+// It is not until n = 23, that a value exceeds one-million:
+//      C(23, 10) = 1144066.
 //
-// How many, not necessarily distinct, values of  nCr, for 1 ≤ n ≤ 100, are
+// How many, not necessarily distinct, values of  C(n, r), for 1 ≤ n ≤ 100, are
 // greater than one-million?
 
 enum { nmax = 100, nmin = 23 };
